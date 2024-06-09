ce9916e82ccacc7f8a524391641a166fa5f84fb5
Donny <nstokes142@gmail.com>
2024-06-09 09:42:55 -0700

    1st commit

---
+++ pages/servertest.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+       
+      <h1> 
+        This is the donnytest page 
+        It calls donny.ts on the server using the useFetch composable   
+        The  server returns a json datatype
+    </h1>
+
+<!-- DISPLAY THE  returned DATA FROM THE SERVER CALL -->
+<!-- <ul>
+  <li>
+    <img :src="scizor.sprite"/>
+  </li>
+  <li>
+    {{ scizor.id }}: {{ scizor.name }}
+  </li>
+  </ul> -->
+{{ data }}
+    </div>
+    
+
+
+  </template>
+
+
+  <script setup>
+  
+  //CALL THE ENDPOINT ON THE SERVER with the UseFetch composable 
+  const { data} = await useFetch(
+    '/api/donny'
+  )
+</script>
+  
---
+++ pages/vidtest.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      <h1>Nuxt 3 Video.js Player</h1>
+      <!-- <VideoJsPlayer videoSrc="https://s3.us-west-1.amazonaws.com/stonetigermedia.com/Bearologist!.mp4" /> -->
+      <VideoJsPlayer videoSrc="https://s3.us-west-1.amazonaws.com/stonetigermedia.com/Lingus.mp4  "/>
+    </div>
+  </template>
+  
+  <script setup>
+  import VideoJsPlayer from '~/components/VideoJsPlayer.vue';
+  </script>
